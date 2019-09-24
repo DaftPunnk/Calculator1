9aa144091b1a6a606e51a32ec611de195b8a3ca3
ZhaokunChen <chenzhaokun@vip.qq.com>
2019-09-24 20:51:38 +0800

    Initial Commit

---
+++ Calculator1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator1
+//
+//  Created by Will Chen on 24/09/19.
+//  Copyright © 2019 Will Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
